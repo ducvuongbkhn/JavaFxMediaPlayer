6f61782905a3cd06990d396c907a15a49875e3f1
Đức Vượng BKHN <ducvuongbkhn@gmail.com>
2015-05-11 09:24:52 +0700

    Đẩy project lên Github

---
+++ MediaPlayer/src/application/MediaPlayerMain.java
@@ -0,0 +1,35 @@
+package application;
+	
+import java.io.File;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+
+
+public class MediaPlayerMain extends Application {
+	
+    Media media = new Media(new File("E:\\Entertainment\\Video\\Doremon\\Doraemon (VTV1 - 2000) Tap 7.mp4").toURI().toString());
+	//public static Media media = new Media("http://download.oracle.com/otndocs/products/javafx/oow2010-2.flv");
+	MediaPlayer mediaPlayer = new MediaPlayer(media);
+	MediaView mediaView = new MediaView(mediaPlayer);
+	
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Embedded Media Player");
+        Group root = new Group();
+        root.getChildren().add(mediaView);
+        mediaPlayer.setAutoPlay(true);      
+        Scene scene = new Scene(root, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
