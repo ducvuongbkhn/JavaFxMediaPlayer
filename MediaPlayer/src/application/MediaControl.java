42b744a2681f8ec333b2131bdeeebbe956edcf0e
Đức Vượng BKHN <ducvuongbkhn@gmail.com>
2015-05-11 19:29:38 +0700

    Tạo khung ứng dụng

--- MediaPlayer/src/application/MediaControl.java
+++ MediaPlayer/src/application/MediaControl.java
@@ -2,16 +2,32 @@ package application;
 
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.DoubleProperty;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import javafx.util.Duration;
 
 public class MediaControl extends BorderPane{
-
-	//timeline
+	
+	//slider bar
+	private MediaPlayer mediaPlayer;
 	private MediaView mediaView;
+	private final boolean repeat = false;
+	private boolean stopRequested = false;
+	private boolean atEndOfMedia = false;
+	private Duration duration;
+	private Slider timeSlider;
+	private Label playTime;
+	private Slider volumeSlider;
+	private HBox mediaBar;
+
 
 	public MediaControl(MediaPlayer mediaPlayer) {
+		
 		setStyle("-fx-background-color: white;");
 		mediaView = new MediaView(mediaPlayer);
 		BorderPane mvPane = new BorderPane() {};
@@ -21,10 +37,22 @@ public class MediaControl extends BorderPane{
 		setFill();
 	}
 	
+	//cai dat autosize
 	private void setFill() {
+		
 		final DoubleProperty width = mediaView.fitWidthProperty();
 		final DoubleProperty height = mediaView.fitHeightProperty();
 		width.bind(Bindings.selectDouble(mediaView.sceneProperty(), "width"));
 		height.bind(Bindings.selectDouble(mediaView.sceneProperty(), "height"));
+	}
+	
+	//Tao slider volume va timeline
+	private HBox sliderBar() {
+		return mediaBar;
+	}
+	
+	//Tao thanh cong cu chua cac button
+	private HBox toolBar() {
+		return mediaBar;
 	}
 }
