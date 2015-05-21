fd6e805d79b2c946aac968d0498077d6f9d4b15c
Đức Vượng BKHN <ducvuongbkhn@gmail.com>
2015-05-22 00:28:40 +0700

    Thay đổi thanh công cụ, gộp thành 1 file

--- MediaPlayer/src/application/MediaPlayerMain.java
+++ MediaPlayer/src/application/MediaPlayerMain.java
@@ -2,35 +2,140 @@ package application;
 	
 import java.io.File;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
 
 
 public class MediaPlayerMain extends Application {
-	
-    Media media = new Media(new File("E:\\Entertainment\\Video\\Doremon\\Doraemon (VTV1 - 2000) Tap 7.mp4").toURI().toString());
+	private Duration duration;
+	private Slider timeSlider;
+	private Label playTime;
+	private Slider volumeSlider;
+	private HBox mediaBar;
+	private HBox toolbar;
+	private boolean FlagFull = false;
+
+	private double w;
+	private double h;
+	Media media = new Media(new File("E:\\Entertainment\\Video\\Walt Disney\\Peter Pan 2.MP4").toURI().toString());
 	//public static Media media = new Media("http://download.oracle.com/otndocs/products/javafx/oow2010-2.flv");
-	MediaPlayer mediaPlayer = new MediaPlayer(media);
-	MediaView mediaView = new MediaView(mediaPlayer);
+	MediaPlayer player = new MediaPlayer(media);
+	MediaView view = new MediaView(player);
+	
+	Color TextColor = Color.rgb(255, 255, 255, 0.5);
+	String ButtonStyle = "-fx-background-color: red;";
 	
 	@Override
 	public void start(Stage primaryStage) {
-		primaryStage.setTitle("Video Media Player");
-        Group root = new Group();
-        mediaPlayer.setAutoPlay(true);      
-        Scene scene = new Scene(root, 800, 600);
-        MediaControl mediaControl = new MediaControl(mediaPlayer);
-        scene.setRoot(mediaControl);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+		primaryStage.setTitle("Coppy Movie Player");
+		primaryStage.setScene(InitScene(primaryStage));
+		//primaryStage.setResizable(false);
+		//primaryStage.initStyle(StageStyle.UTILITY);  //HIDDEn
+		primaryStage.show();
+		setFill();
+		player.play();
+		
 	}
 	
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	private Scene InitScene(Stage primaryStage) {
+		Group root = new Group();
+		BorderPane mvPane = new BorderPane(root);
+		mvPane.setStyle("-fx-background-color: black;");
+		VBox vbox = new VBox();	
+		vbox.setAlignment(Pos.BOTTOM_CENTER);
+		vbox.setPadding(new Insets(5, 10, 5, 10));
+		//vbox.getChildren().add(mediaBar());
+		//vbox.getChildren().add(toolBar(primaryStage));
+		Scene scene = new Scene(mvPane, 1067, 600, Color.BLACK);
+		root.getChildren().add(view);
+		root.getChildren().add(vbox);
+		
+		//hieu ung cho timeline
+		final Timeline slideIn = new Timeline();
+		final Timeline slideOut = new Timeline();
+		root.setOnMouseEntered(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent mouseEvent) {
+				slideIn.play();
+			}
+		});
+		root.setOnMouseExited(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent mouseEvent) {
+				slideOut.play();
+			}
+		});
+		
+		player.setOnReady(new Runnable() {
+			@Override
+			public void run() {
+				duration = player.getMedia().getDuration();
+				updateSize(primaryStage);
+				primaryStage.setMinWidth(w);
+				primaryStage.setMinHeight(h);
+				vbox.setMinSize(w, 100);
+				vbox.setTranslateY(h - 100);
+
+				slideOut.getKeyFrames().addAll(
+						new KeyFrame(new Duration(0),
+								new KeyValue(vbox.translateYProperty(), h-100),
+								new KeyValue(vbox.opacityProperty(), 0.9)
+								),
+								new KeyFrame(new Duration(300),
+										new KeyValue(vbox.translateYProperty(), h),
+										new KeyValue(vbox.opacityProperty(), 0.0)
+										)
+						);
+				slideIn.getKeyFrames().addAll(
+						new KeyFrame(new Duration(0),
+								new KeyValue(vbox.translateYProperty(), h),
+								new KeyValue(vbox.opacityProperty(), 0.0)
+								),
+								new KeyFrame(new Duration(300),
+										new KeyValue(vbox.translateYProperty(), h-100),
+										new KeyValue(vbox.opacityProperty(), 0.9)
+										)
+						);
+			}
+		});
+		return scene;
+	}
+	
+	public void setFill() {
+		final DoubleProperty width = view.fitWidthProperty();
+		final DoubleProperty height = view.fitHeightProperty();
+		width.bind(Bindings.selectDouble(view.sceneProperty(), "width"));
+		height.bind(Bindings.selectDouble(view.sceneProperty(), "height"));
+	}
+	
+	
+	private void updateSize(Stage primaryStage) {
+		w = primaryStage.getScene().getWidth();
+		h = primaryStage.getScene().getHeight();
 	}
 }
